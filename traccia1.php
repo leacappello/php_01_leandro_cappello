7514041cf1ab9930cb272b3c0cf1cbf3a6051612
leacappello <81253331+leacappello@users.noreply.github.com>
2026-03-17 12:47:37 +0100

    Primo esercizio PHP

---
+++ traccia1.php
@@ -0,0 +1,29 @@
+<?php
+
+// Variabili
+$age = 25;
+$height = 1.75;
+$greetingMessage = "Ciao a tutti";
+$isLogged = true;
+
+// Stampa tipo di dato
+echo gettype($age) . "\n";
+echo gettype($height) . "\n";
+echo gettype($greetingMessage) . "\n";
+echo gettype($isLogged) . "\n";
+
+echo "\n";
+
+// Costanti
+const AGE = 25;
+const HEIGHT = 1.75;
+const GREETING_MESSAGE = "Ciao a tutti";
+const IS_LOGGED = true;
+
+// Stampa tipo di dato costanti
+echo gettype(AGE) . "\n";
+echo gettype(HEIGHT) . "\n";
+echo gettype(GREETING_MESSAGE) . "\n";
+echo gettype(IS_LOGGED) . "\n";
+
+?>
