c736014d57a59dade63cd6dc50e277360cddce84
leacappello <81253331+leacappello@users.noreply.github.com>
2026-03-20 15:03:50 +0100

    Aggiunta traccia 2

---
+++ traccia2.php
@@ -0,0 +1,15 @@
+<?php
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perche'";
+$text6 = $text2;
+$text7 = "bevuto";
+$text8 = "tutto";
+
+// Stampa frase finale
+echo $text1 . " Hai " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8 . ".";
+
+?>
